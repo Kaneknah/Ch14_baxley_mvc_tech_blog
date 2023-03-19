521b739d735fd23d83603db3952d9e732e14ab05
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-19 17:34:48 -0600

    updated to allow OnDelete CASCADE

--- models/index.js
+++ models/index.js
@@ -2,21 +2,19 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
-
 //Post models setup
 Post.belongsTo(User, {
 	foreignKey: "user_id",
+	onDelete: "CASCADE",
 });
 Post.hasMany(Comment, {
 	foreignKey: "post_id",
+	onDelete: "CASCADE",
 });
 //Comments models setup
 Comment.belongsTo(User, {
 	through: Post,
 	foreignKey: "user_id",
 });
-
-
-
 
 module.exports = { User, Post, Comment };
