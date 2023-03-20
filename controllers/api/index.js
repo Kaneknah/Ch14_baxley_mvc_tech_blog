71e4ec7d2eeb957912bb0a0af6f290addac9de25
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-19 18:18:27 -0600

    Index controlers  reated.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
-const userRoutes = require("./userRoutes.js");
-const postRoutes = require("./postRoutes.js");
-const commentRoutes = require("./commentRoutes.js");
+const userRoutes = require("./userRoute");
+const postRoutes = require("./postRoute");
+const commentRoutes = require("./commentRoute");
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes.js");
-const dashboardRoutes = require("./dashboardRoutes.js");
+const dashboardRoutes = require("./dashboardRoutes");
 
 router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/", homeRoutes);
-
-router.use((req, res) => {
-	res.status(404).end();
-});
 
 module.exports = router;
