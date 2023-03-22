cb800eeabb893fc621167f76a725c32a6cd4a4fc
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-21 18:42:56 -0600

    router.get for "/" path

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,7 +1,45 @@
-const router = require ("express").Router();
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
+//get route to find all posts.
+router.get("/", withAuth, (req, res) => {
+	Post.findAll({
+		where: {
+			user_id: req.session.user_id,
+		},
+		attributes: ["id", "title", "body", "created_at"],
+		include: [
+			{
+				model: Comment,
+				attributes: ["id", "comment_body", "post_id", "user_id", "created_at"],
+				include: {
+					model: User,
+					attributes: ["username"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["username"],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			const posts = dbPostData.map((post) =>
+				post.get({
+					plain: true,
+				})
+			);
+			res.render("dashboard", {
+				posts,
+				loggedIn: true,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
-
-
-
-module.exports = router
+module.exports = router;
